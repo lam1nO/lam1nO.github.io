8c6e0d611ff362bee55c7c55f339f2c6581d29ae
lam1nO <61313428+lam1nO@users.noreply.github.com>
2020-08-21 20:10:26 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -41,53 +41,35 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
-var card = document.querySelectorAll(".spec__wrapper");
-const cards1 = document.querySelector(".spec__wrapper1");
-const cards2 = document.querySelector(".spec__wrapper2");
-const cards3 = document.querySelector(".spec__wrapper3");
-const cards4 = document.querySelector(".spec__wrapper4");
-const cards5 = document.querySelector(".spec__wrapper5");
-const cards6 = document.querySelector(".spec__wrapper6");
+const cards = document.querySelectorAll(".spec__wrapper");
 
-var i
+cards.forEach(card => {
+    card.addEventListener("click", function(){func(card)});    
+});
 
-for (i = 0; i < card.length; i++)
+function func(card)
 {
-    console.log(card[i]);
-    card[i].addEventListener('click', function()
-    {
-        card[i].classList.add("spec__wrapper--clicked");
-    });
+    card.classList.toggle("spec__wrapper--clicked");
+    console.log(card);
 }
 
-// cards1.addEventListener('click', function(){func(cards1)});
-// cards2.addEventListener('click', function(){func(cards2)});
-// cards3.addEventListener('click', function(){func(cards3)});
-// cards4.addEventListener('click', function(){func(cards4)});
-// cards5.addEventListener('click', function(){func(cards5)});
-// cards6.addEventListener('click', function(){func(cards6)});
-
-const hi = document.querySelector(".hello");
-
-hi.addEventListener('click', back);
-
-function func(cards){
-    console.log(cards);
-    cards.classList.add("spec__wrapper--clicked");
+var myswiper = new Swiper(".swiper-container", {
+    speed: 400, 
+    spaceBetween: 100,
+    direction: 'horizontal',
+    loop: true,
     
-    // cards.classList.remove("spec__wrapper--clicked");
-}
-
-function back(){
-    hi.parentElement.classList.remove("spec__wrapper--clicked");
-    console.log(hi.parentElement);
-}
-
-// for (i = 0; i < cards.length; i+=1)
-// {    
-//     cards[i].addEventListener("click", func(cards[i]));
-// }
-
-// function func(x){
-//     x.classList.add("spec__wrapper--clicked");
-// }
+    pagination: {
+        el : '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    autoplay:{
+        delay: 2000,
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
