2b111824ce7d2bc84178e9b1b01d289dfcc8f70e
lam1nO <61313428+lam1nO@users.noreply.github.com>
2020-12-02 11:36:38 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,32 +1,41 @@
-/* Индекс слайда по умолчанию */
+// const cards = document.querySelectorAll(".spec__wrapper");
 
+// cards.forEach(card => {
+//     card.addEventListener("click", function(){func(card)});    
+// });
 
-const cards = document.querySelectorAll(".spec__wrapper");
+// function func(card)
+// {
+//     card.classList.toggle("spec__wrapper--clicked");
+//     console.log(card);
+// }
 
-cards.forEach(card => {
-    card.addEventListener("click", function(){func(card)});    
-});
+var dialog = document.querySelector('.spec__info-800');
+var overlay = document.querySelector('.overlay');
+document.querySelector('#spec__info-open-800').onclick = function () {
+    dialog.show();
+    overlay.classList.add('overlay-open');
+};
+document.querySelector('#spec__info-close-800').onclick = function () {
+    dialog.close();
+    overlay.classList.remove('overlay-open')
+};
 
-function func(card)
-{
-    card.classList.toggle("spec__wrapper--clicked");
-    console.log(card);
-}
 
 var myswiper = new Swiper(".swiper-container", {
-    speed: 400, 
+    speed: 400,
     spaceBetween: 100,
     direction: 'horizontal',
     loop: true,
-    
+
     pagination: {
-        el : '.swiper-pagination',
+        el: '.swiper-pagination',
     },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
-    autoplay:{
+    },
+    autoplay: {
         delay: 7000,
     },
     scrollbar: {
